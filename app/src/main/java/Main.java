bd0b2ff43d403d0f0baae79fddfd47711c549180
Murakami Aota <e225737@ie.u-ryukyu.ac.jp>
2022-12-20 13:06:09 +0900

    第一段階ザックリコーディング

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,5 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args){
-        System.out.println("Hello,aotyo's World!");
+        int sleepTime = 2500;
+        
+        Random rand = new Random();
+        int val1 = rand.nextInt(99);
+        int val2 = rand.nextInt(99);
+        int val3 = rand.nextInt(99);
+        int val4 = rand.nextInt(99);
+        int val5 = rand.nextInt(99);
+        int answer = val1 + val2 + val3+ val4+ val5;
+
+        try{
+            System.out.println("フラッシュ暗算、やってみよう！");
+            Thread.sleep(1000);
+            System.out.println("準備OK？");
+
+            Thread.sleep(3000);
+            System.out.println(val1);
+            System.out.println("\n\n\n");
+            Thread.sleep(sleepTime);
+            System.out.println(val2);
+            System.out.println("\n\n\n");
+            Thread.sleep(sleepTime);
+            System.out.println(val3);
+            System.out.println("\n\n\n");
+            Thread.sleep(sleepTime);
+            System.out.println(val4);
+            System.out.println("\n\n\n");
+            Thread.sleep(sleepTime);
+            System.out.println(val5);
+            Thread.sleep(sleepTime);
+            System.out.println("答えはいくつ？");
+
+            int myAnswer = new Scanner(System.in).nextInt();
+
+            if (myAnswer == answer){
+                System.out.println("あたり");
+            } else{
+                System.out.println("答えは"+answer+"でした");
+            }
+        } catch (InterruptedException e) {
+		}
+
     }
 }
